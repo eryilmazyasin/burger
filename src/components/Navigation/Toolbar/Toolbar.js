0c76f7e0f8e36bc7d1bbfd6dbc6e9f6528a204a9
Yasin Eryılmaz <yasineryilmazfb@gmail.com>
2020-08-21 17:27:52 +0300

    navigation items added

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
  function Toolbar(props) {
     return (
@@ -9,7 +10,7 @@ import Logo from '../../Logo/Logo';
                 <div>MENU</div>
                 <Logo />
                 <nav>
-                    ...
+                    <NavigationItems />
                 </nav>
             </header>
     )
